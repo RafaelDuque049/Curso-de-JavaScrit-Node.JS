e2e571d16a17946030cf2110920ced7c28edc3e0
Rafael_Duque <rafaellduque049@gmail.com>
2022-11-06 00:10:35 -0300

    Condições
    Entendendo como funciona as condições do JavaScript.

---
+++ JavaScript Node.js/aula11/ex008.js
@@ -0,0 +1,11 @@
+// Conteúdo: Condição.
+
+let vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+
+if (vel > 60){ // Condição simples
+    console.log('Você recebeu uma multa por andar acima da velocidade permitida.')
+}
+
+console.log('Dirija sempre usando o cinto de segurança para seu bem estar.')
---
+++ JavaScript Node.js/aula11/ex009.js
@@ -0,0 +1,11 @@
+// Conteúdo: Condição.
+
+let pais = 'Eua'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é estrangeiro.')
+}
