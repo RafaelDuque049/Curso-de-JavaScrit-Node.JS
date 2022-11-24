1835870781e02b0ef8b8a78ae36a84057d27712a
Rafael_Duque <rafaellduque049@gmail.com>
2022-11-23 23:49:45 -0300

    Funcionalidades da página
    Colocando em prática as funcionalidades da página da atividade.

---
+++ JavaScript Node.js/Aula12ex/ex0015/script.js
@@ -0,0 +1,31 @@
+function check(){
+    const data = new Date().getFullYear()
+
+    let ano = document.querySelector('.Ano_nascido').value
+
+    let result = document.querySelector('.result')
+
+    if (ano.length == 0 || ano > data){
+        alert('//Erro//  Descrições incorretas, tente novamente.')
+        
+    } else {
+        let img = document.createElement("img")
+        
+        let fsex = document.getElementsByName('sex_select')
+
+        const genero = fsex[0].checked ? 'Homem' : 'Mulher'
+
+        const idade = (data - ano);
+
+        img.setAttribute('id', 'foto')
+
+        let imagem = document.querySelector('img#foto')
+
+        if (idade >= 0 && idade < 10){
+            imagem.src = (genero == 'Homem' ? 'bebe_M.jpg' : '/bebe.jpg')
+        }
+
+        result.style.textAlign = 'center'
+        result.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    }
+}
