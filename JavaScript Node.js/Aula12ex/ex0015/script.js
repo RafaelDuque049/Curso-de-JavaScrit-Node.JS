172c4207464a678a407cfdc85ce51866f2e23585
Rafael_Duque <rafaellduque049@gmail.com>
2022-11-25 23:15:59 -0300

    Atividade finalizada.
    Foi finalizado a atividade 15 da aula 12.

--- JavaScript Node.js/Aula12ex/ex0015/script.js
+++ JavaScript Node.js/Aula12ex/ex0015/script.js
@@ -1,29 +1,43 @@
-function check(){
-    const data = new Date().getFullYear()
+// Verificador de idade e sexo.
 
+function check(){
     let ano = document.querySelector('.Ano_nascido').value
 
     let result = document.querySelector('.result')
 
-    if (ano.length == 0 || ano > data){
-        alert('//Erro//  Descrições incorretas, tente novamente.')
+    const data = new Date().getFullYear()
+
+    const idade = (data - ano)
+
+    if (ano.length == 0 || ano > data || idade > 125) {
+        return alert('//Erro//  Descrições incorretas, tente novamente.')
         
     } else {
         let img = document.createElement("img")
         
-        let fsex = document.getElementsByName('sex_select')
-
+        const fsex = document.getElementsByName('sex_select')
+        
         const genero = (fsex[0].checked ? 'Homem' : 'Mulher')
-
-        const idade = (data - ano);
-
+        
         img.setAttribute('id', 'foto')
 
-        if (idade >= 0 && idade < 10){
-            img.setAttribute('src', (genero == 'Homem' ? 'bebe_M.jpg' : 'bebe.jpg'))
-        }
+        if (idade >= 0 && idade <= 10) {
+            img.setAttribute('src', (genero == 'Homem' ? 'images/bebe_M.jpg' : 'images/bebe.jpg'))
+        
+        } else if (idade <= 21) {
+            img.setAttribute('src', (genero == 'Homem' ? 'images/jovem_M.jpg' : 'images/jovem_F.jpg'))
+        
+        } else if (idade <= 50) {
+            img.setAttribute('src', (genero == 'Homem' ? 'images/homem.jpg' : 'images/mulher.jpg'))
+        
+        } else {
+            img.setAttribute('src', (genero == 'Homem' ? 'images/idoso.jpg' : 'images/idosa.jpg'))
+        };
 
-        result.style.textAlign = 'center'
-        result.innerHTML = `Detectamos ${genero} com ${idade} anos`
-    }
-}
+        result.style.textAlign = 'center';
+        
+        result.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+
+        result.appendChild(img);
+    };
+};
