d4933fa6a352d543c4a6f32f11de607fcaf03b3b
Rafael_Duque <rafaellduque049@gmail.com>
2022-11-27 00:58:37 -0300

    Corrigindo problemas.
    Foi feita a correção de um pequeno problema de digitação.
    Motivo do problema: das 00 hrs até 01 hrs da manhã, não seria atualizada o fundo e os demais itens por engano no momento de digitação de condição.

--- JavaScript Node.js/Aula12ex/ex014/Script.js
+++ JavaScript Node.js/Aula12ex/ex014/Script.js
@@ -9,7 +9,7 @@ function eventos(msg, back, image){
 function time_page(){
     const text = document.querySelector('.time');
 
-    let time_fixed = 0;
+    let time_fixed = null;
 
     setInterval(() => {
         const time_now = new Date();
@@ -17,22 +17,22 @@ function time_page(){
         const time_hours = time_now.getHours();
 
         if (time_hours != time_fixed){
-            time_fixed = time_hours
-
             if (time_hours < 6){
                 eventos("Tenha uma boa madrugada!!", 'rgb(19, 1, 102)', 'images/madrugada.png')
-    
+                
             } else if (time_hours < 12){
                 eventos("Tenha um ótimo dia!!", 'rgba(255, 180, 51, 0.798)', 'images/manha.png')
-        
+                
             } else if (time_hours < 18){
                 eventos("Tenha uma ótima Tarde!!", 'rgb(255, 132, 10)', 'images/tarde.png')
-        
+                
             } else {
                 eventos("Tenha uma boa noite!!", 'rgb(107, 107, 241)', 'images/noite.png')
             }
+            
+            time_fixed = time_hours
         };
-    
+        
         text.innerHTML = `Horário: ${time_now.getHours()}:${time_now.getMinutes()}:${time_now.getSeconds()}`;
     }, 500);
 };
