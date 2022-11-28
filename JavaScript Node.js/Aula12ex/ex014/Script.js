d0bddc7fcc9f2d026eec781498f41cb1c35e9e7e
Rafael_Duque <rafaellduque049@gmail.com>
2022-11-27 23:58:18 -0300

    Novo conteúdo
    Criando arquivos para o novo conteúdo na aula 13

--- JavaScript Node.js/Aula12ex/ex014/Script.js
+++ JavaScript Node.js/Aula12ex/ex014/Script.js
@@ -1,12 +1,14 @@
-function eventos(msg, back, image){
-    document.querySelector('.msg').innerHTML = msg
 
-    document.body.style.background = back
+function time_page() {
 
-    document.querySelector('.imagem').src = image
-}
+    function eventos(msg, back, image) {
+        document.querySelector('.msg').innerHTML = msg
+    
+        document.body.style.background = back
+    
+        document.querySelector('.imagem').src = image
+    }
 
-function time_page(){
     const text = document.querySelector('.time');
 
     let time_fixed = null;
@@ -16,12 +18,17 @@ function time_page(){
 
         const time_hours = time_now.getHours();
 
-        if (time_hours != time_fixed){
-            if (time_hours < 6){
+        if (time_hours != time_fixed) {
+             
+            if (time_hours < 6) {
                 eventos("Tenha uma boa madrugada!!", 'rgb(19, 1, 102)', 'images/madrugada.png')
+
+                document.querySelector('#box_itens').style.boxShadow = '5px 5px 10px rgb(204 0 255 / 71%)'
                 
             } else if (time_hours < 12){
                 eventos("Tenha um ótimo dia!!", 'rgba(255, 180, 51, 0.798)', 'images/manha.png')
+
+                document.querySelector('#box_itens').style.boxShadow = '5px 5px 10px rgba(0, 0, 0, 0.713)'
                 
             } else if (time_hours < 18){
                 eventos("Tenha uma ótima Tarde!!", 'rgb(255, 132, 10)', 'images/tarde.png')
@@ -30,6 +37,8 @@ function time_page(){
                 eventos("Tenha uma boa noite!!", 'rgb(107, 107, 241)', 'images/noite.png')
             }
             
+            document.querySelector('.dia').innerHTML = `Dia ${time_now.getDate()}`
+
             time_fixed = time_hours
         };
         
