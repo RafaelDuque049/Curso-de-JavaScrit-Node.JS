a9b19aafbc31cc6c8313e938989c774f88bbc62f
Rafael_Duque <rafaellduque049@gmail.com>
2022-12-02 20:28:48 -0300

    Estrutura de repetições
    Aprendendo a usar as estruturas de repetições While e Do-While.

---
+++ JavaScript Node.js/Aula13/ambiente.js
@@ -0,0 +1,23 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+let contador = 1;
+
+/* Estrutura de repetição de teste logíco no inicio.
+while (contador <= 6) {
+    console.log('Tudo bem?')
+    ++contador
+};
+*/
+
+// Estrutura de repetição de teste lógico no final
+do {
+    console.log('Tudo bem?')
+    --contador
+} while (contador <= 6);
