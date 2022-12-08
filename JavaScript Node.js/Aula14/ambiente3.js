9fd9b853533beefef752f8fe72021adf4f5b2047
Rafael_Duque <rafaellduque049@gmail.com>
2022-12-07 23:19:46 -0300

    Praticando conteúdo
    Utilizando WHILE e FOR para entender mais a diferenças entre suas estruturas.

---
+++ JavaScript Node.js/Aula14/ambiente3.js
@@ -0,0 +1,13 @@
+// fazendo a utilização das estruturas de repetições WHILE e FOR.
+
+let num = 0;
+
+while (num <= 10) {
+    console.log(num)
+    num++
+}
+
+for (let num2 = num;num2 >= 0; num2 -= 1) {
+    console.log(num)
+    num--
+}
